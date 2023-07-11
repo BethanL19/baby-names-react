a6b5b39459b9908a01d684052d32c017f2f60d12
Beth <bethan.hp@gmail.com>
2023-07-11 11:56:34 +0100

    favs bar added & working

--- src/baby-names.tsx
+++ src/baby-names.tsx
@@ -3,14 +3,14 @@ import { NamesDataInfo, namesData } from "./babyNamesData";
 
 interface BabyNameButtonProps {
   nameData: NamesDataInfo;
-  handleClickedFavs: (name: string) => void;
+  handleClickedFavs: (item: NamesDataInfo) => void;
 }
 
 function BabyNameButton(props: BabyNameButtonProps): JSX.Element {
   return (
     <>
       <button
-        onClick={() => props.handleClickedFavs(props.nameData.name)}
+        onClick={() => props.handleClickedFavs(props.nameData)}
         className={"nameButtons" + props.nameData.sex}
       >
         {props.nameData.name}
@@ -23,20 +23,29 @@ function BabyNames(): JSX.Element {
   const [typedSearch, setTypedSearch] = useState("");
   const handleSearch = (searchWord: string) => setTypedSearch(searchWord);
 
-  const [favName, setFavName] = useState<string>("");
-  const [favNamesList, setFaveNamesList] = useState<string[]>([]);
-  const handleClickedFavs = (name: string) => {
-    setFaveNamesList([...favNamesList, favName]);
-    setFavName(name);
+  const [favNamesList, setFaveNamesList] = useState<NamesDataInfo[]>([]);
+  const handleClickedFavs = (item: NamesDataInfo) => {
+    if (!favNamesList.includes(item)) {
+      setFaveNamesList([...favNamesList, item]);
+    }
+  };
+  const handleUnFavourite = (item: NamesDataInfo) => {
+    const index = favNamesList.indexOf(item);
+    favNamesList.splice(index, 1);
+    setFaveNamesList([...favNamesList]);
   };
 
   let filteredNamesData;
   if (typedSearch.length > 0) {
-    filteredNamesData = namesData.filter((item) =>
-      item.name.toLowerCase().includes(typedSearch.toLowerCase())
+    filteredNamesData = namesData.filter(
+      (item) =>
+        item.name.toLowerCase().includes(typedSearch.toLowerCase()) &&
+        !favNamesList.includes(item)
     );
   } else {
-    filteredNamesData = namesData;
+    filteredNamesData = namesData.filter(
+      (item) => !favNamesList.includes(item)
+    );
   }
 
   const sortedNamesData = [...filteredNamesData].sort((a, b) =>
@@ -61,8 +70,14 @@ function BabyNames(): JSX.Element {
       />
       <h3 className="favs">
         Favourites:{" "}
-        {favNamesList.slice(1).map((n, index) => (
-          <button key={index}>{n}</button>
+        {favNamesList.map((item, index) => (
+          <button
+            className={"nameButtons" + item.sex}
+            key={index}
+            onClick={() => handleUnFavourite(item)}
+          >
+            {item.name}
+          </button>
         ))}
       </h3>
       {namesArray}
